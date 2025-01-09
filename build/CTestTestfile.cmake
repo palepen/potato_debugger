abfc076dd6407d60d028b0d643be095588d15bb0
palelpen <122384892+palepen@users.noreply.github.com>
2025-01-09 23:03:13 +0530

    dlt build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunless/projects/potato_debugger
-# Build directory: /home/sunless/projects/potato_debugger/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tools")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sunless/projects/potato_debugger/test
-# Build directory: /home/sunless/projects/potato_debugger/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
